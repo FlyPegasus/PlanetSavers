ae91855acee76e1ac70f0c926a06ee176a013de7
Anshu Das <anshusignin@gmail.com>
2024-12-21 13:05:06 +0530

    removed inference sdk

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 from flask import Flask, render_template, request, url_for, flash, redirect
 import os
-from inference_sdk import InferenceHTTPClient
 import cv2
 import plastic_detector
 
@@ -50,38 +49,6 @@ def plastic_view():
     return render_template("plastic.html", title="plastic")
 
 
-#testing
-@app.route("/test", methods=['GET', 'POST'])
-def test_view():
-    if request.method == 'POST':
-        # Check if the POST request has the file part
-        if 'file' not in request.files:
-            flash('No file part')
-            return redirect(request.url)
-        
-        file = request.files['file']
-
-        # If the user does not select a file, browser may submit an empty file part
-        if file.filename == '':
-            flash('No selected file')
-            return redirect(request.url)
-        if file and allowed_file(file.filename):
-            filename = file.filename
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            flash('File successfully uploaded')
-            file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
-            CLIENT = InferenceHTTPClient(
-                api_url="https://detect.roboflow.com",
-                api_key="jfbwoJ5zSnkPddWB4rRA"
-            )
-
-            result = CLIENT.infer(file_path, model_id="tree-counting-qiw3h/1")
-            cv2.imwrite(os.path.join(app.config['PROCESSED_FOLDER'] , 'waka.jpg'), result)
-            #cv2.waitKey(0)
-        else:
-            flash('File not supported!')
-    
-    return render_template("modeltest.html")
 
 
 if __name__=="__main__":
